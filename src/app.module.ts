f95dfa1f4096887dc18b8ecb362ea904d7d0a8c5
Jozef Janecek <jozef.janecek@asseco-ceit.com>
2024-02-02 20:51:39 +0100

    add env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { AppService } from './app.service';
 import { TempService } from './temp.service';
 import { SCD30Service } from './scd30.service';
 import { GPIOService } from './gpio.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, TempService, SCD30Service,GPIOService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: '*'})
-
+  const configService = app.get<ConfigService>(ConfigService);
+  const appPort = configService.get<number>('NEST_PORT', 3001);
+  const appHost = configService.get<string>('NEST_HOST', '127.0.0.1');
+  
   const config = new DocumentBuilder()
   .setTitle('Smart Striga')
   .setDescription('The cats API description')
@@ -14,8 +17,11 @@ async function bootstrap() {
   
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  
+  app.enableCors({origin: '*'})
+  await app.listen(appPort, appHost);
 
-  await app.listen(3000);
+  console.log(`\n🚀 Server started and listening on: http://${appHost}:${appPort}`)
 }
 
-bootstrap();
+bootstrap()
